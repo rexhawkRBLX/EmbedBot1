96b744d1ed33c75f1e840695704618b025530e78
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-11-28 14:36:54 -0500

    init

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const http = require("http");
-const app = require("express")();
+const bot_config = require("./botconfig.json");
 const Discord = require("discord.js");
+const fs = require("fs");
 const PORT = process.env.PORT || 3000;
+const app = require("express")();
 const bot = new Discord.Client({disableEveryone: true});
-
+const prefix = bot_config.prefix;
+bot.commands = new Discord.Collection();
+fs.readdir("./commands/", (err, files) =>{
+    if(err) console.log(err);
+    let jsfile = files.filter(f => f.split(".").pop() === "js");
+    if (jsfile.length <= 0){
+        return console.log("Couldnt find commands.");
+    }
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+    });
+});
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
-
-bot.on("ready", async () => {
-    bot.channels.get('588879387475312660').send(`Bot is online running on port \`${PORT}\`.`);
+bot.on("ready", async() => {
+    bot.channels.get('588879387475312660').send(`EmbedBot is online back online running on port \`${PORT}\``);
     bot.user.setActivity("Broome County, California", {type: "PLAYING"});
 });
-
+bot.on("message", async message => {
+   if (message.author.bot) return;
+   if (message.channel.type === "dm") return;
+   let messageArray = message.content.split(" ");
+   let cmd = messageArray[0].toLowerCase();
+   let args = messageArray.slice(1);
+   // Functions
+   if (message.content.startsWith(prefix)){
+       let commandFile = bot.commands.get(cmd.slice(prefix.length));
+       if (commandFile) await commandFile.run(bot,message,args);
+   }
+});
 // Prevent exit 143 (Idle exit)
 function startKeepAlive() {
     setInterval(function() {
         let options = {
-            host: 'kingcity-bot.herokuapp.com',
+            host: 'broomeembedbot.herokuapp.com',
             port: 80,
             path: '/'
         };
